bbbf41587254a63b28c20e0709eb4fe3fee1c0d4
USay <csys21001@g.nihon-u.ac.jp>
2023-10-03 13:46:55 +0900

    made example of MyPage

--- cafe-app/src/pages/MyPage.tsx
+++ cafe-app/src/pages/MyPage.tsx
@@ -1,8 +1,25 @@
+import { getAuth, onAuthStateChanged } from '@firebase/auth'
+import { useState } from 'react'
+
+// TODO: write status of each user with uid and load/save functions.
+// TODO: make UI
 
 const Mypage = () => {
+  const auth = getAuth()
+  const [user, setUser] = useState<string>()
+  onAuthStateChanged(auth, (user) => {
+    if(user) {
+      const name = user.displayName
+      if (name) setUser(name)
+    }
+  })
+
   return (
     <>
     This page is my page.
+    <div>
+      {user}
+    </div>
     </>
   )
 }
