cdc24c5057d79212965ba752a466b88d1fbe30ea
USay <csys21001@g.nihon-u.ac.jp>
2023-12-12 06:03:54 +0900

    change GFormURL component

--- cafe-app/src/pages/api/getGFormUrl.ts
+++ cafe-app/src/pages/api/getGFormUrl.ts
@@ -5,22 +5,9 @@ export const getGFormURLWithInitValue = (
   const formID = "1FAIpQLScDyGluCCHPhD6ij4gqdUmfixnstnD1DJzFtz0Y4Zsda1533g";
   // formのエントリIDは調べて打ち込むこと
   const entryCategoryID = "1638938702";
-  const entryTeishokuID = "640761078";
-  const entryNoodleID = "1766694091";
-  const entryDonID = "697295449";
-  const entryCurryID = "942483614";
-  const entryMenuNameID =
-    category === "定食"
-      ? entryTeishokuID
-      : category === "麺類"
-      ? entryNoodleID
-      : category === "丼"
-      ? entryDonID
-      : category === "カレー"
-      ? entryCurryID
-      : undefined;
+  const entryID = "640761078";
 
-  const entryURL = `&entry.${entryCategoryID}=${category}&entry.${entryMenuNameID}=${menuName}`;
+  const entryURL = `&entry.${entryCategoryID}=${category}&entry.${entryID}=${menuName}`;
   const rootURL = `https://docs.google.com/forms/d/e/${formID}/viewform?usp=pp_url`;
   const mainURL = encodeURI(rootURL + entryURL);
   console.log(`re-direct to ${mainURL}`);
